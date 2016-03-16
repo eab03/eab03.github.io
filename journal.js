2e294ba8b10862f530d7b9a9231f131975279635
eab03 <elizabethbright@Elizabeths-MacBook-Air.local>
2016-03-15 22:19:25 -0400

    3.12 notes and 3.15 code transfer from pen
    Added a quick note from 3.12 section and copied over coding from
    Codepen to incorporate the sticky nav bar.

--- journal.js
+++ journal.js
@@ -1,19 +1,35 @@
-var header = document.getElementById('header');
+var nav = document.getElementById('main_nav');
+var navOriginalDistanceFromTop = nav.offsetTop;
 
-window.onscroll = function(event) {
+window.addEventListener('scroll', function() {
 
-    make_navbar_sticky();
+  var amountScrolled = document.body.scrollTop;
 
-        function make_navbar_sticky() {
-        // creates navbar below splash page map which sticks to top of screen when page is scrolled past map
-        var nav = document.getElementById('main_nav');
-        
-        if (getComputedStyle(header).getPropertyValue('display') !== 'none' && header.getBoundingClientRect().bottom <= 0) {
-            nav.style.position = 'fixed';
-            nav.style.top = '0';
-        } else {
-            console.log(!header.style.display + "    " + header.getBoundingClientRect().bottom);
-            nav.style.position = 'relative';
-        }
-    };
-}
+  if (amountScrolled <= navOriginalDistanceFromTop && nav.classList.contains('sticky')) {
+    nav.classList.toggle('sticky')
+  } else if (amountScrolled >= navOriginalDistanceFromTop && !nav.classList.contains('sticky')) {
+    nav.classList.toggle('sticky')
+  }
+});
+
+//Old Code: 
+
+//var main_nav = document.getElementById('main_nav');
+
+//window.onscroll = function() {
+//make_navbar_sticky();
+
+//var navPosition = main_nav.getBoundingClientRect().top;
+//var amountScrolled = document.body.scrollTop;
+//console.log(amountScrolled);
+//console.log(navPosition);
+
+//if ((amountScrolled >= navPosition) || (navPosition == 0)) {
+//main_nav.classList.toggle('sticky');
+//} else {
+//main_nav.classList.remove('sticky');
+//}
+//};
+
+// notes to self: look up scrollTop and offsetTop
+// make nav bar sticky by adding class of sticky

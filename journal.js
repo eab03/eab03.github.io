389f248ee5239a8ee51cf48bdebf6989cde8f21d
eab03 <elizabethbright@Elizabeths-MacBook-Air.local>
2016-03-28 21:12:38 -0400

    3.28 updates
    Updated code from pen. Tried to add additional js functionality to the
    navigation. Commented out the code tried to add, and reverted back to a
    modified version of the old code for now.

--- journal.js
+++ journal.js
@@ -12,27 +12,42 @@ window.addEventListener('scroll', function() {
   }
 });
 
+// var button1 = document.querySelector('#buttonjan'); // Grab the button
+// // So that we can add an event listener for when we click it
+// button1.addEventListener('click', function() {
+//   // Which first uses `getBoundingClientRect` (https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect) to figure out where the top of the target is
+// var targetTop = document.querySelector('#january').getBoundingClientRect().top;
+//   // Define an offset (how much room we want to leave above our target when we scroll)
+//   var offset = 110;
+//   // And then use `scrollTo` (https://developer.mozilla.org/en-US/docs/Web/API/Window/scroll) to actually scroll the page, but scroll and leave `offset` room above our target
+//   window.scrollTo(0, targetTop - offset);
+// });
+
+// var button2 = document.querySelector('#buttonfeb'); // Grab the button
+// // So that we can add an event listener for when we click it
+// button2.addEventListener('click', function() {
+//   // Which first uses `getBoundingClientRect` (https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect) to figure out where the top of the target is
+//   var targetTop = document.querySelector('#february').getBoundingClientRect().top;
+//   // Define an offset (how much room we want to leave above our target when we scroll)
+//   var offset = 110;
+//   // And then use `scrollTo` (https://developer.mozilla.org/en-US/docs/Web/API/Window/scroll) to actually scroll the page, but scroll and leave `offset` room above our target
+//   window.scrollTo(0, targetTop - offset);
+// });
+
+// var button3 = document.querySelector('#buttonmarch'); // Grab the button
+// // So that we can add an event listener for when we click it
+// button3.addEventListener('click', function() {
+//   // Which first uses `getBoundingClientRect` (https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect) to figure out where the top of the target is
+//   var targetTop = document.querySelector('#march').getBoundingClientRect().top;
+//   // Define an offset (how much room we want to leave above our target when we scroll)
+//   var offset = 110;
+//   // And then use `scrollTo` (https://developer.mozilla.org/en-US/docs/Web/API/Window/scroll) to actually scroll the page, but scroll and leave `offset` room above our target
+//   window.scrollTo(0, targetTop - offset);
+// });
 
 
 
 
-// Original JavaScript code by Chirp Internet: www.chirp.com.au
-// Please acknowledge use of this code by including this header.
-
-//http://www.the-art-of-web.com/javascript/remove-anchor-links/
-// document.addEventListener("DOMContentLoaded", function() {
-//   var links = document.getElementsByTagName("A");
-//   for(var i=0; i < links.length; i++) {
-//     if(!links[i].hash) continue;
-//     if(links[i].origin + links[i].pathname != self.location.href) continue;
-//     (function(anchorPoint) {
-//       links[i].addEventListener("click", function(e) {
-//         anchorPoint.scrollIntoView(true);
-//         e.preventDefault();
-//       }, false);
-//     })(document.getElementById(links[i].hash.replace(/#/, "")));
-//   }
-// }, false);
 
 
 
